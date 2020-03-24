6235a6a3166a417ccade69cbec8b3e22b2bba9b5
shiopon01 <shio.0323@gmail.com>
2020-03-24 18:27:33 +0900

    chore: コメント変更

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,12 +8,11 @@ export const app: express.Express = express();
  */
 app.use("/", routes);
 
-console.log("ENV", process.env.NODE_ENV);
-
 // NOTE: supertestを動かす際にapp.listenするとテストが終わらなくなる
 if (process.env.NODE_ENV !== "test") {
   const port = process.env.PORT ? process.env.PORT : 3001;
   app.listen(port, () => {
-    console.log(`listen port: ${port}`);
+    console.log(`env : ${process.env.NODE_ENV}`);
+    console.log(`port: ${port}`);
   });
 }

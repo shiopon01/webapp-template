bb9836c53d973a07036470864a3fd5397ce5932f
shiopon01 <shio.0323@gmail.com>
2020-04-03 00:48:36 +0900

    feat: local & bearerのエラーをキャッチするミドルウェア

--- backend/src/controllers/auth_controller.ts
+++ backend/src/controllers/auth_controller.ts
@@ -1,25 +1,14 @@
-import express from "express";
-import passport from "passport";
 import IORedis from "ioredis";
 import options from "../configs/redis";
 
-export const login = (req: express.Request, res: express.Response, next: any): void => {
-  passport.authenticate("local", { session: false }, async (err: any, user: any, _info: any) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.status(400).json({ message: "authentication failed" });
-    }
-    res.status(200).json({ user });
-  })(req, res, next);
+export const login = (req: any, res: any) => {
+  // ミドルウェアでログイン処理までしているため、ログイン成功済みのリクエストだけたどり着く
+  res.status(200).json({ message: "success", user: req.user });
 };
 
 export const logout = (req: any, res: any) => {
-  console.log("User", req.user.accessToken20);
-
   const redis = new IORedis(options);
-  redis.del(`token:access:${req.user.accessToken20}`)
+  redis.del(`token:access:${req.user.accessToken20}`);
   redis.quit();
 
   res.status(200).json({ message: "success" });
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,10 +3,53 @@ import passport from "passport";
 import * as auth from "./controllers/auth_controller";
 import * as echo from "./controllers/echo_controller";
 
+// Passportのlocal認証を行うミドルウェア。Passportに帰依するエラーはHTTPレスポンスを返し、通常のエラーはnext(err)
+// NOTE: username, passwordどちらかが不足している場合にStrategyが勝手に返す401 Unauthorizedをキャッチ
+const local = (req: any, res: any, next: any): void => {
+  passport.authenticate("local", { session: false }, async (err: any, user: any, _info: any) => {
+    if (err) {
+      if (err.status === 401) {
+        // 認証情報が違う理由で不正な場合
+        return res.status(err.status).json({ message: "invalid token" });
+      }
+      return next(err);
+    }
+    if (!user) {
+      // username, passwordどちらかが不足している場合
+      return res.status(400).json({ message: "username and password required" });
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
+};
+
+// passportのbearer認証を行うミドルウェア。Passportに帰依するエラーはHTTPレスポンスを返し、通常のエラーはnext(err)
+// NOTE: bearerが不足している場合にStrategyが勝手に返す401 Unauthorizedをキャッチ
+// NOTE: https://qiita.com/uasi/items/cfb60588daa18c2ec6f5
+const bearer = (req: any, res: any, next: any): void => {
+  passport.authenticate("bearer", { session: false }, async (err: any, user: any, _info: any) => {
+    if (err) {
+      if (err.status === 401) {
+        // トークンが失効や破損などの理由で不正な場合
+        res.set("www-authenticate", 'Bearer error="invalid_token"');
+        return res.status(err.status).json({ message: "invalid token" });
+      }
+      return next(err);
+    }
+    if (!user) {
+      // bearerが不足している場合
+      res.set("www-authenticate", 'Bearer realm="token_required"');
+      return res.status(401).json({ message: "token required" });
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
+};
+
 const routes = expressPromiseRouter();
 
-routes.post("/login", auth.login);
-routes.get("/logout", passport.authenticate("bearer", { session: false }), auth.logout);
+routes.post("/login", local, auth.login);
+routes.get("/logout", bearer, auth.logout);
 
 routes.get("/echo", echo.index);
 
